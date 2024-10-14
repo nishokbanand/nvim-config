a1dfa1f62cfe639080084ed8656ccff75530e353
B, Nishok A <nishok_anand@optum.com>
2024-10-14 19:33:50 +0530

    updated config for trouble

--- lua/nba/plugins/trouble.lua
+++ lua/nba/plugins/trouble.lua
@@ -1,8 +1,7 @@
 return {
   "folke/trouble.nvim",
-  config = function()
-    require("trouble").setup {
-      icons = false,
+  opts = {
+    icons = {
       fold_open = "v",      -- icon used for open folds
       fold_closed = ">",    -- icon used for closed folds
       indent_lines = false, -- add an indent guide below the fold icons
@@ -16,11 +15,36 @@ return {
       },
       use_diagnostic_signs = false -- enabling this will use the signs defined in your lsp client
     }
-    vim.keymap.set("n", "<leader>xd", function() require("trouble").toggle("document_diagnostics") end,
-      { desc = "[T]oggle document diagnostics" })
-    vim.keymap.set("n", "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end,
-      { desc = "[T]oggle workspace_diagnostics" })
-    vim.keymap.set("n", "[d", function() require("trouble").next({ skip_groups = true, jump = true }) end)
-    vim.keymap.set("n", "]d", function() require("trouble").previous({ skip_groups = true, jump = true }) end)
-  end
+  },
+  cmd  = "Trouble",
+  -- config = function()
+  --   require("trouble").setup({
+  --     icons = false,
+  --     fold_open = "v",      -- icon used for open folds
+  --     fold_closed = ">",    -- icon used for closed folds
+  --     indent_lines = false, -- add an indent guide below the fold icons
+  --     signs = {
+  --       -- icons / text used for a diagnostic
+  --       --{ Error = " ", Warn = " ", Hint = " ", Info = "" }
+  --       error = "",
+  --       warning = "",
+  --       hint = "󰠠",
+  --       information = ""
+  --     },
+  --     use_diagnostic_signs = false -- enabling this will use the signs defined in your lsp client
+  --   })
+  -- vim.keymap.set("n", "<leader>xd", function() require("trouble").toggle() end,
+  --   { desc = "[T]oggle document diagnostics" })
+  -- -- vim.keymap.set("n", "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end,
+  -- --   { desc = "[T]oggle workspace_diagnostics" })
+  -- vim.keymap.set("n", "[d", function() require("trouble").next({ skip_groups = true, jump = true }) end)
+  -- vim.keymap.set("n", "]d", function() require("trouble").previous({ skip_groups = true, jump = true }) end)
+  -- end,
+  keys = {
+    {
+      "<leader>xx",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics (Trouble)",
+    },
+  }
 }
