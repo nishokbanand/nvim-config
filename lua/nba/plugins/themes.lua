322b122a4a41f8786297bccb4c49fddb3fa1acf2
B, Nishok A <nishok_anand@optum.com>
2024-10-14 19:34:45 +0530

    added tmux config

--- lua/nba/plugins/themes.lua
+++ lua/nba/plugins/themes.lua
@@ -1,9 +1,16 @@
 return {
   -- "rebelot/kanagawa.nvim",
-  "catppuccin/nvim",
-  priority = 1000,
-  init = function()
-    vim.cmd.colorscheme 'catppuccin'
-    vim.cmd.hi 'Comment gui=none'
-  end,
+  -- "catppuccin/nvim",
+  -- "craftzdog/solarized-osaka.nvim",
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    priority = 1000,
+    config = function()
+      require("rose-pine").setup({})
+
+      -- setup must be called before loading
+      vim.cmd.colorscheme "rose-pine"
+    end,
+  }
 }
